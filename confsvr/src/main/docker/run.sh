87e703d35bd1ddd1b62b0b6671bbc8b7d7589aa7
jgkim <jgkim@cybermed.co.kr>
2019-01-17 16:48:56 +0900

    bootstrap

--- confsvr/src/main/docker/run.sh
+++ confsvr/src/main/docker/run.sh
@@ -3,3 +3,4 @@ echo "********************************************************"
 echo "Starting Configuration Server"
 echo "********************************************************"
 java -jar /usr/local/configserver/@project.build.finalName@.jar
+
